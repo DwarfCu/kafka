96965c118e8a02fa678b9b677396c619ae894e70
DwarfCu <joserrahervas@gmail.com>
2018-07-19 15:52:29 +0200

    Tests added

--- kafka-streams/src/test/java/com/dwarfcu/kafka/streams/RemoveDuplicatedLinksTests.java
+++ kafka-streams/src/test/java/com/dwarfcu/kafka/streams/RemoveDuplicatedLinksTests.java
@@ -1,7 +1,6 @@
 package com.dwarfcu.kafka.streams;
 
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
