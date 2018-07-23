d1e49c24d567b17e5f212d6629dc27bcb3246b66
Dwarfcu <joserrahervas@gmail.com>
2018-07-23 08:50:57 +0200

    rename class

--- kafka-streams/src/test/java/com/dwarfcu/kafka/streams/CompactLinksTests.java
+++ kafka-streams/src/test/java/com/dwarfcu/kafka/streams/CompactLinksTests.java
@@ -16,7 +16,7 @@ import java.util.Properties;
 
 import static org.junit.Assert.assertEquals;
 
-public class RemoveDuplicatedLinksTests {
+public class CompactLinksTests {
   private TopologyTestDriver topologyTestDriver;
 
   private StringSerializer stringSerializer = new StringSerializer();
