68fd43904c2c0ee951b513e4a77d193966c71320
Dwarfcu <joserrahervas@gmail.com>
2018-09-27 13:54:52 +0200

    Mockaroo Kafka Producer

--- mockaroo/src/main/java/com/dwarfcu/mockaroo/kafka/MockarooProducer.java
+++ mockaroo/src/main/java/com/dwarfcu/mockaroo/kafka/MockarooProducer.java
@@ -12,9 +12,6 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.lang.reflect.Method;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Properties;
 
 public class MockarooProducer {
@@ -95,11 +92,6 @@ public class MockarooProducer {
 
   private static Object get(String property) {
     return properties.get(property);
-  }
-
-  private static String readFile(String path, Charset encoding) throws IOException {
-    byte[] encoded = Files.readAllBytes(Paths.get(path));
-    return new String(encoded, encoding);
   }
 
   private static String capitalizeFirstLetter(String original) {
